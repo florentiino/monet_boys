bb29b2d4c71609add4be6160a2d60d08c3abbc7e
Amine El Amrani <amine.elamrani.epfl@gmail.com>
2021-08-24 16:36:14 +0200

    Generator improved

--- monet_boys/generator.py
+++ monet_boys/generator.py
@@ -27,76 +27,141 @@ from tensorflow.keras import layers
 import tensorflow_addons as tfa
 
 
-class Generator():
-    def __init__(self):
-        """
-        to be defined
-        """
+# Global variables
 
-    def downsample(filters, size, apply_instancenorm=True):
-        """ defines and returns a downsampler layer with parameters :
-        filter (dimensionality of output space)
-        size (kernel size)
-        (+ optionally applies instance normalization)
+OUTPUT_CHANNELS = 3
 
-        """
-        #Initialize random distributions for the kernel initializer and the instance normalizer
-        initializer = tf.random_normal_initializer(0., 0.02)
-        gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
-        # Instantiate model
-        result = keras.Sequential()
+def downsample(filters, size, apply_instancenorm=True):
+    """ defines and returns a downsampler layer with parameters :
+    filter (dimensionality of output space)
+    size (kernel size)
+    (+ optionally applies instance normalization)
 
-        # Add 2D convolutional layer w. stride 2 and padding
-        result.add(layers.Conv2D(filters, size, strides=2, padding='same',
-                                kernel_initializer=initializer, use_bias=False))
+    """
+    #Initialize random distributions for the kernel initializer and the instance normalizer
+    initializer = tf.random_normal_initializer(0., 0.02)
+    gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
 
-        # Apply instance normalization
-        if apply_instancenorm:
-            result.add(tfa.layers.InstanceNormalization(gamma_initializer=gamma_init))
+    # Instantiate model
+    result = keras.Sequential()
 
-        # Add a last Leaky ReLU layer
-        result.add(layers.LeakyReLU())
+    # Add 2D convolutional layer w. stride 2 and padding
+    result.add(layers.Conv2D(filters, size, strides=2, padding='same',
+                            kernel_initializer=initializer, use_bias=False))
 
-        return result
-
-    def upsample(filters, size, apply_dropout=False):
-        """ defines and returns an upsampler layer with parameters :
-            filter (dimensionality of output space)
-            size (kernel size)
-            (+ optionally applies droupout)
-
-        """
-        #Initialize random distributions for the kernel initializer and the instance normalizer
-        initializer = tf.random_normal_initializer(0., 0.02)
-        gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
-
-        # Instantiate model
-        result = keras.Sequential()
-
-        # Add transposed 2D convolutional layer w. stride 2 and padding
-        result.add(layers.Conv2DTranspose(filters, size, strides=2,
-                                        padding='same',
-                                        kernel_initializer=initializer,
-                                        use_bias=False))
-
-        # Apply instance normalization
+    # Apply instance normalization
+    if apply_instancenorm:
         result.add(tfa.layers.InstanceNormalization(gamma_initializer=gamma_init))
 
-        # Apply droupout
-        if apply_dropout:
-            result.add(layers.Dropout(0.5))
+    # Add a last Leaky ReLU layer
+    result.add(layers.LeakyReLU())
 
-        # Add a last ReLU layer
-        result.add(layers.ReLU())
+    return result
 
-        return result
+def upsample(filters, size, apply_dropout=False):
+    """ defines and returns an upsampler layer with parameters :
+        filter (dimensionality of output space)
+        size (kernel size)
+        (+ optionally applies droupout)
 
+    """
+    #Initialize random distributions for the kernel initializer and the instance normalizer
+    initializer = tf.random_normal_initializer(0., 0.02)
+    gamma_init = keras.initializers.RandomNormal(mean=0.0, stddev=0.02)
+
+    # Instantiate model
+    result = keras.Sequential()
+
+    # Add transposed 2D convolutional layer w. stride 2 and padding
+    result.add(layers.Conv2DTranspose(filters, size, strides=2,
+                                    padding='same',
+                                    kernel_initializer=initializer,
+                                    use_bias=False))
+
+    # Apply instance normalization
+    result.add(tfa.layers.InstanceNormalization(gamma_initializer=gamma_init))
+
+    # Apply droupout
+    if apply_dropout:
+        result.add(layers.Dropout(0.5))
+
+    # Add a last ReLU layer
+    result.add(layers.ReLU())
+
+    return result
+
+
+
+def Generator():
+    """ downsamples then upsamples an image
+    while establishing skip connections (to handle vanishing gradient) from downsample output
+    to upsample layer in symetrical fashion
+    --> returns a Keras model
+    """
+
+    # Define input layer + shape
+    inputs = layers.Input(shape=[256,256,3])
+
+    # Instantiate down stack using downsample function previously defined
+    # bs = batch size
+    down_stack = [
+        downsample(64, 4, apply_instancenorm=False), # (bs, 128, 128, 64)
+        downsample(128, 4), # (bs, 64, 64, 128)
+        downsample(256, 4), # (bs, 32, 32, 256)
+        downsample(512, 4), # (bs, 16, 16, 512)
+        downsample(512, 4), # (bs, 8, 8, 512)
+        downsample(512, 4), # (bs, 4, 4, 512)
+        downsample(512, 4), # (bs, 2, 2, 512)
+
+        downsample(512, 4), # (bs, 1, 1, 512) # Last layer of the down stack, won't have skip connection with up_stack
+    ]
+
+    # Instantiate down stack using downsample function previously defined
+    up_stack = [
+        upsample(512, 4, apply_dropout=True), # (bs, 2, 2, 1024)
+        upsample(512, 4, apply_dropout=True), # (bs, 4, 4, 1024)
+        upsample(512, 4, apply_dropout=True), # (bs, 8, 8, 1024)
+        upsample(512, 4), # (bs, 16, 16, 1024)
+        upsample(256, 4), # (bs, 32, 32, 512)
+        upsample(128, 4), # (bs, 64, 64, 256)
+        upsample(64, 4), # (bs, 128, 128, 128)
+    ]
+
+    # Add last layer that will uspample the previous layer to a final 256x256x3 image
+    initializer = tf.random_normal_initializer(0., 0.02)
+    last = layers.Conv2DTranspose(OUTPUT_CHANNELS, 4,
+                                  strides=2,
+                                  padding='same',
+                                  kernel_initializer=initializer,
+                                  activation='tanh') # (bs, 256, 256, 3)
+
+
+    ## Establish skip connections
+    x = inputs
+
+    # Downsampling through the model
+    skips = []
+    for down in down_stack:
+        x = down(x)
+        skips.append(x)
+
+    skips = reversed(skips[:-1])
+
+    # Upsampling and establishing the skip connections
+    for up, skip in zip(up_stack, skips):
+        x = up(x)
+        x = layers.Concatenate()([x, skip])
+
+    x = last(x)
+
+    # Return a keras model 
+    return keras.Model(inputs=inputs, outputs=x)
 
 
 if __name__ == "__main__":
 
     # Instantiate one Generator
     a = Generator()
-    print(type(a))
+    print(a.summary())
 
